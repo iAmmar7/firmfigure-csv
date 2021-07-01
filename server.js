3d61d46151c2bc0fc6ef1497579ac39bb486de33
iammar7 <maansaridot@gmail.com>
2021-07-01 14:10:32 +0500

    not serving FE from Node

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userAuth, userRoutes);
 
 // Serve Frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
